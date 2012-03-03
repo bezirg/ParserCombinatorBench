e4137462727a719c2be73c452b1a8d777eb69c76
Nikolaos Bezirgiannis <bezirg@gmail.com>
2012-03-03 12:03:16 +0100

    NF - WHNF infrastructure

---
+++ src/Parsing/Bench/BenchNF.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE CPP #-}
+
+module Main where
+
+#include "Parsers.h"
+
+import Criterion.Main
+import Data.Attoparsec.Text
+
+perror = error "Parsing Failed"
+
+fromDone (Done _ x) = x
+fromDone _ = perror
+
+fromRight (Right x) = x
+fromRight _ = perror
+
+fromFst (x,[]) = x
+fromFst _ = perror
+
+main = do
+  -- Lazy load the inputs
+  inpCV <- readFile "data/csv/bench.csv"
+  inpEX <- readFile "data/expr/bench.expr"
+  inpUR <- readFile "data/url/bench.url"
+  inpHT <- readFile "data/http/bench.http"
+  inpJS <- readFile "data/json/bench.json"
+  inpCS <- readFile "data/css/bench.css"
+
+  defaultMain $ [
+        bgroup "csv" [
+                    bench "AP" $ nf (fromDone . APCV.run) inpCV
+                   ,bench "P2" $ nf (fromRight . P2CV.run) inpCV
+                   ,bench "P3" $ nf (fromRight . P3CV.run) inpCV
+                   ,bench "U1" $ nf (fromRight . U1CV.run) inpCV
+                   ,bench "U2" $ nf (fromFst . U2CV.run) inpCV
+                   ,bench "PP" $ nf (fromRight . fromFst . PPCV.run) inpCV
+                   ]
+       ,bgroup "expr" [
+                    bench "AP" $ nf (fromDone . APEX.run) inpEX
+                   ,bench "P2" $ nf (fromRight . P2EX.run) inpEX
+                   ,bench "P3" $ nf (fromRight . P3EX.run) inpEX
+                   ,bench "U1" $ nf (fromRight . U1EX.run) inpEX
+                   ,bench "U2" $ nf (fromFst . U2EX.run) inpEX
+                   ,bench "PP" $ nf (fromRight . fromFst . PPEX.run) inpEX
+                   ]
+       ,bgroup "url" [
+                    bench "AP" $ nf (fromDone . APUR.run) inpUR
+                   ,bench "P2" $ nf (fromRight . P2UR.run) inpUR
+                   ,bench "P3" $ nf (fromRight . P3UR.run) inpUR
+                   ,bench "U1" $ nf (fromRight . U1UR.run) inpUR
+                   ,bench "U2" $ nf (fromFst . U2UR.run) inpUR
+                   ,bench "PP" $ nf (fromRight . fromFst . PPUR.run) inpUR
+                   ]
+       ,bgroup "http" [
+                    bench "AP" $ nf (fromDone . APHT.run) inpHT
+                   ,bench "P2" $ nf (fromRight . P2HT.run) inpHT
+                   ,bench "P3" $ nf (fromRight . P3HT.run) inpHT
+                   ,bench "U1" $ nf (fromRight . U1HT.run) inpHT
+                   ,bench "U2" $ nf (fromFst . U2HT.run) inpHT
+                   ,bench "PP" $ nf (fromRight . fromFst . PPHT.run) inpHT
+                   ]
+       ,bgroup "json" [
+                    bench "AP" $ nf (fromDone . APJS.run) inpJS
+                   ,bench "P2" $ nf (fromRight . P2JS.run) inpJS
+                   ,bench "P3" $ nf (fromRight . P3JS.run) inpJS
+                   ,bench "U1" $ nf (fromRight . U1JS.run) inpJS
+                   ,bench "U2" $ nf (fromFst . U2JS.run) inpJS
+                   ,bench "PP" $ nf (fromRight . fromFst . PPJS.run) inpJS
+                   ]
+       ,bgroup "css" [
+                    bench "AP" $ nf (fromDone . APCS.run) inpCS
+                   ,bench "P2" $ nf (fromRight . P2CS.run) inpCS
+                   ,bench "P3" $ nf (fromRight . P3CS.run) inpCS
+                   ,bench "U1" $ nf (fromRight . U1CS.run) inpCS
+                   ,bench "U2" $ nf (fromFst . U2CS.run) inpCS
+                   ,bench "PP" $ nf (fromRight . fromFst . PPCS.run) inpCS
+                   ]
+
+
+       ]
