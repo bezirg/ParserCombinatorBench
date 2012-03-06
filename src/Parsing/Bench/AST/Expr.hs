80c4976fb8a99608294bf0bbff1460abc49e0f94
Nikolaos Bezirgiannis <bezirg@gmail.com>
2012-03-06 12:16:20 +0100

    Disabled QuickCheck for Expr, the datasets are made

--- src/Parsing/Bench/AST/Expr.hs
+++ src/Parsing/Bench/AST/Expr.hs
@@ -1,7 +1,7 @@
 module Parsing.Bench.AST.Expr where
 
-import Test.QuickCheck.Arbitrary
-import Test.QuickCheck.Gen
+-- import Test.QuickCheck.Arbitrary
+-- import Test.QuickCheck.Gen
 
 import Control.Monad
 import Control.DeepSeq
@@ -33,23 +33,23 @@ instance NFData Expr where
 --     show (Var s) = s
 --     show (Let x e1 e2) = "let " ++ x ++ " = " ++ show e1 ++  " in " ++ show e2
 
-instance Arbitrary Expr where
-    arbitrary = sized arbitraryExpr 
+-- instance Arbitrary Expr where
+--     arbitrary = sized arbitraryExpr 
 
 
-arbitraryId = listOf1 (choose ('a','z'))
-arbitraryN = liftM abs arbitrary
+-- arbitraryId = listOf1 (choose ('a','z'))
+-- arbitraryN = liftM abs arbitrary
 
-arbitraryExpr 0 = oneof [liftM Nat arbitraryN, liftM Var arbitraryId]
-arbitraryExpr n = frequency [(1, liftM Nat arbitraryN),
-                             (1, liftM Var arbitraryId),
-                             (3, liftM3 Let arbitraryId arbitraryE arbitraryE),
-                             (3, liftM2 Add arbitraryE arbitraryE),
-                             (3, liftM2 Sub arbitraryE arbitraryE),
-                             (3, liftM2 Mul arbitraryE arbitraryE)
-                          ]
-                  where 
-                    arbitraryE = arbitraryExpr (n-1)
+-- arbitraryExpr 0 = oneof [liftM Nat arbitraryN, liftM Var arbitraryId]
+-- arbitraryExpr n = frequency [(1, liftM Nat arbitraryN),
+--                              (1, liftM Var arbitraryId),
+--                              (3, liftM3 Let arbitraryId arbitraryE arbitraryE),
+--                              (3, liftM2 Add arbitraryE arbitraryE),
+--                              (3, liftM2 Sub arbitraryE arbitraryE),
+--                              (3, liftM2 Mul arbitraryE arbitraryE)
+--                           ]
+--                   where 
+--                     arbitraryE = arbitraryExpr (n-1)
 
-genExprs = sample' (arbitrary :: Gen Expr)
+-- genExprs = sample' (arbitrary :: Gen Expr)
 
